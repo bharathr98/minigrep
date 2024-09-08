306a099e2883fab56b52385c4ee685a8b390680d
Bharath Radhakrishnan <r.bharathkumar@outlook.com>
2024-09-08 16:24:15 +0200

    Encode and decode char to ANSII

--- src/main.c
+++ src/main.c
@@ -1,5 +1,17 @@
 #include<stdio.h>
 #include<string.h>
+const char ascii_table[] = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+int char_to_ascii(int ch)
+{
+    const char *p = strchr(ascii_table, ch);
+    return p ? p - ascii_table + 32 : 0;
+}
+
+int ascii_to_char(int a)
+{
+     return (a >= 32 && a < 128) ? ascii_table[a-32] : 0;
+}
 int main(int argc, char **argv){
     char* stringT = argv[1];
     char* pattern = argv[2];
